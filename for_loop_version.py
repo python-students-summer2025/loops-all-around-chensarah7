50116274ad5947cf82205e48728e7c6affbe2747
Sarah C <chensarah7@gmail.com>
2025-06-24 17:43:07 -0700

    completed assignment

---
+++ for_loop_version.py
@@ -0,0 +1,86 @@
+'''
+using for loop
+'''
+
+def get_starting_number():
+    '''
+    ask for input until user response if any integer 1 or greater.\n
+    returns integer equivalent of value user entered.
+    '''
+    stay_on_this = True
+    while stay_on_this:
+        user_raw = input('How many bottles of beer of the wall? ')
+        user_str_prep = str(user_raw)
+        user_str = user_str_prep.strip()
+        if user_str.isnumeric() and user_str != '0':
+            stay_on_this = False
+        else:
+            stay_on_this = True
+    user = int(user_str)
+    return user
+
+def sing(initial:int):
+    '''
+    takes argument specifying how many bottles to start with.\n
+    outputs lyrics to the song, starting from passed num of bottles.
+    '''
+
+    # create the list to iterate from
+    x = initial
+    num_list = [x]
+    while x > 1:
+        # y = x - 1
+        num_list.append(x - 1)
+        # num_line.append(y)
+        x = x - 1
+
+    # print(num_list)
+    x = initial
+
+    for x in range(initial, 0, -1):
+        if x > 2:
+            print(f'{x} bottles of beer on the wall, {x} bottles of beer.')
+            print(f'Take one down, pass it around, {x-1} bottles of beer on the wall.')
+            print('')
+            x = x - 1
+        elif x == 2:
+            print(f'{x} bottles of beer on the wall, {x} bottles of beer.')
+            print(f'Take one down, pass it around, {x-1} bottle of beer on the wall.')
+            print('')
+            x = x - 1
+        else:
+            print(f'{x} bottle of beer on the wall, {x} bottle of beer.')
+            print('Take it down, pass it around, no more bottles of beer on the wall!')
+            x = x - 1
+
+def sing_v1(initial:int):
+    '''
+    takes argument specifying how many bottles to start with.*\n*
+    outputs lyrics to the song, starting from passed num of bottles.
+    '''
+
+    # create the list to iterate from
+    x = initial
+    num_list = [x]
+    while x > 1:
+        # y = x - 1
+        num_list.append(x - 1)
+        # num_line.append(y)
+        x = x - 1
+
+
+    #
+    # print(num_list)
+    #
+    x = initial
+
+    for x in num_list:
+        if x > 1:
+            print(f'{x} bottles of beer on the wall, {x} bottles of beer.')
+            print(f'Take one down, pass it around, {x-1} bottles of beer on the wall.')
+            print('')
+            x = x - 1
+        else:
+            print(f'{x} bottle of beer on the wall, {x} bottle of beer.')
+            print('Take it down, pass it around, no more bottles of beer on the wall!')
+            x = x - 1
---
+++ while_loop_accumulator_version.py
@@ -0,0 +1,45 @@
+'''
+using while loop controlled by accumulator variable
+'''
+
+#
+def get_starting_number():
+    '''
+    ask for input until user response if any integer 1 or greater.\n
+    returns integer equivalent of value user entered.
+    '''
+    stay_on_this = True
+    while stay_on_this:
+        user_raw = input('How many bottles of beer of the wall? ')
+        user_str_prep = str(user_raw)
+        user_str = user_str_prep.strip()
+        if user_str.isnumeric() and user_str != '0':
+            stay_on_this = False
+        else:
+            stay_on_this = True
+    user = int(user_str)
+    return user
+
+def sing(initial):
+    '''
+    takes argument specifying how many bottles to start with.\n
+    outputs lyrics to the song, starting from passed num of bottles.
+    '''
+
+    x = initial
+
+    while x >= 1:
+        if x > 2:
+            print(f'{x} bottles of beer on the wall, {x} bottles of beer.')
+            print(f'Take one down, pass it around, {x-1} bottles of beer on the wall.')
+            print('')
+            x = x - 1
+        elif x == 2:
+            print(f'{x} bottles of beer on the wall, {x} bottles of beer.')
+            print(f'Take one down, pass it around, {x-1} bottle of beer on the wall.')
+            print('')
+            x = x - 1
+        else:
+            print(f'{x} bottle of beer on the wall, {x} bottle of beer.')
+            print('Take it down, pass it around, no more bottles of beer on the wall!')
+            x = x - 1
